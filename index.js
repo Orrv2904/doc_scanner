0036586e9f1e3dcf88b4f52087390bd5b2f915a7
Omar Rodríguez <82064182+Orrv2904@users.noreply.github.com>
2023-05-25 20:51:27 +0000

    :construction:

--- index.js
+++ index.js
@@ -49,25 +49,6 @@ function captureImage() {
   const demoImages = document.getElementById('demo-images');
   demoImages.appendChild(imageContainer);
 
-  $('#demo-images .image-container').click(function () {
-    $('.image-container.selected').removeClass('selected')
-    $(this).addClass('selected')
-    const imageSrc = $(this).find('img').data('url')
-    loadOpenCV(function () {
-      $('#demo-result').empty()
-
-      const newImg = document.createElement("img")
-      newImg.src = imageSrc
-
-      newImg.onload = function () {
-        const resultCanvas = scanner.extractPaper(newImg, 386, 500);
-        $('#demo-result').append(resultCanvas);
-
-        const highlightedCanvas = scanner.highlightPaper(newImg)
-        $('#demo-result').append(highlightedCanvas);
-      }
-    })
-  })
 }
 
 
