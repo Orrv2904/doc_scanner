db47f61f5a96a1a53454a6868ba457c846796c1d
Omar Rodríguez <82064182+Orrv2904@users.noreply.github.com>
2023-05-25 13:25:52 -0600

    :construction:

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Obtener referencias a los elementos del DOM
 const openBtn = document.getElementById('openBtn');
+const captureBtn = document.getElementById('captureBtn');
 const closeBtn = document.getElementById('closeBtn');
 const videoElement = document.getElementById('videoElement');
+const canvasElement = document.getElementById('canvasElement');
 let stream;
 
 // Función para abrir la cámara
@@ -16,6 +18,26 @@ async function openCamera() {
   }
 }
 
+// Función para tomar una captura de imagen
+function captureImage() {
+  // Establecer el tamaño del lienzo igual al tamaño del video
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+  
+  // Dibujar el video en el lienzo
+  const context = canvasElement.getContext('2d');
+  context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+  
+  // Obtener la imagen capturada en formato base64
+  const imageData = canvasElement.toDataURL('image/png');
+  
+  // Mostrar la imagen capturada en una nueva ventana o en la consola
+  const image = new Image();
+  image.src = imageData;
+  const w = window.open("");
+  w.document.write(image.outerHTML);
+}
+
 // Función para cerrar la cámara
 function closeCamera() {
   // Detener el flujo de video
@@ -28,4 +50,5 @@ function closeCamera() {
 
 // Asignar eventos a los botones
 openBtn.addEventListener('click', openCamera);
+captureBtn.addEventListener('click', captureImage);
 closeBtn.addEventListener('click', closeCamera);
